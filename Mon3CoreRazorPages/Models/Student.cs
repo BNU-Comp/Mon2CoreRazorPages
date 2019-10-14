38a15a84ad6f1cb02040c7cc1649fdbafb21fe77
Derek Peacock <derek_peacock@yahoo.com>
2019-10-14 11:32:30 +0100

    Added a data model

---
+++ Mon3CoreRazorPages/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mon3CoreRazorPages.Models
+{
+    public class Student
+    {
+        // Database Attributes
+        public int StudentID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation Properties (relationships)
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
