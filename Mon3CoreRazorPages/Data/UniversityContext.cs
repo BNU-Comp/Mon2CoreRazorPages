e2df47cef6aca473fffad2fc29fe5bb65971ab5b
Derek Peacock <derek_peacock@yahoo.com>
2019-10-14 11:42:15 +0100

    Added CRUD pages for students

---
+++ Mon3CoreRazorPages/Data/UniversityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mon3CoreRazorPages.Models
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext (DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mon3CoreRazorPages.Models.Student> Student { get; set; }
+    }
+}
---
+++ Mon3CoreRazorPages/Pages/Students/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Mon3CoreRazorPages.Models;
+
+namespace Mon3CoreRazorPages.Pages.Students
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Mon3CoreRazorPages.Models.UniversityContext _context;
+
+        public CreateModel(Mon3CoreRazorPages.Models.UniversityContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Student.Add(Student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
